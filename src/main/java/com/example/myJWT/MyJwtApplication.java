c2ad1399887ac6dcb89e2023fd9e95beef63d97e
whipbaek <jibaek0513@gmail.com>
2023-01-15 16:14:58 +0900

    initial commit

---
+++ src/main/java/com/example/myJWT/MyJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.myJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJwtApplication.class, args);
+	}
+
+}
